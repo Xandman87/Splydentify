789cb3acee6eaef19223ed2affce1a306b80eb42
Michel Heiniger <michel.heiniger@gmail.com>
2011-12-05 14:38:52 +0100

    plot working

---
+++ plotMarkov.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+import sys
+import json
+from pylab import *
+
+class PlotMarkov():
+
+    def __init__(self, filename1, filename2):
+        self.filename1 = filename1
+        self.filename2 = filename2
+        f = open(self.filename1, 'r')
+        self.values1 = json.loads(f.read())
+        g = open(self.filename2, 'r')
+        self.values2 = json.loads(g.read())
+
+    def plot(self):
+        hist(self.values1, 50, normed=1, facecolor='y', alpha=0.5)
+        hist(self.values2, 50, normed=1, facecolor='b', alpha=0.5)
+        title('Title')
+        xlabel('x_label')
+        ylabel('yLabel')
+        show()
+
+if __name__ == '__main__':
+    pm = PlotMarkov(str(sys.argv[1]), str(sys.argv[2]))
+    pm.plot()
